792f246a2cf612635afeb4791932f35d53ca059e
Brandon Chu <brandon@tunezy.com>
2013-03-07 19:15:39 -0500

    End of chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp2::Application.routes.draw do
-
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   root :to => 'static_pages#home'
 
   match '/signup', to: 'users#new'
+  match '/somecrap', to: 'users#edit'
+
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy'
+
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
